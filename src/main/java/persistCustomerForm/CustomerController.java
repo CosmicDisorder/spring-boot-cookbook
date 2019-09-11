f41fd820a10da64cb7aa34b19037e057538f5b6f
Michael Harris <mike.ghazi.harris@gmail.com>
2019-09-10 20:19:19 -0500

    integrated mysql into customerpersist using JPA stuff I still don't fully understand

--- src/main/java/persistCustomerForm/CustomerController.java
+++ src/main/java/persistCustomerForm/CustomerController.java
@@ -1,5 +1,6 @@
 package persistCustomerForm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CustomerController {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @GetMapping("/customer-form")
     public String customerForm(Model model) {
         model.addAttribute("customerPersist", new CustomerPersist());
         return "customer-form";
     }
     @PostMapping("/customer-form")
-    public String customerSubmit(@ModelAttribute CustomerPersist customerPersist) {return "customer-result";}
+    public String customerSubmit(@ModelAttribute CustomerPersist customerPersist) {
+        CustomerPersist newCustomer = customerPersist;
+        customerRepository.save(newCustomer);
+        return "customer-result";}
 }
--- src/main/java/persistCustomerForm/CustomerRepository.java
+++ src/main/java/persistCustomerForm/CustomerRepository.java
@@ -2,9 +2,13 @@ package persistCustomerForm;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
+@Transactional
 public interface CustomerRepository extends CrudRepository<CustomerPersist, Long> {
 
     List<CustomerPersist> findByLastName(String lastName);
