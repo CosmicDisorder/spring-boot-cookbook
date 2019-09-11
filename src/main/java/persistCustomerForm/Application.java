6f7b1f1675b1d3f80e44d2f0076c01d6b9425059
CosmicDisorder <mike.ghazi.harris@gmail.com>
2019-09-11 15:20:06 -0500

    broke then reinstalled and reconfigured mamp, made minimal changes along the way

--- src/main/java/persistCustomerForm/Application.java
+++ src/main/java/persistCustomerForm/Application.java
@@ -1,5 +1,9 @@
 package persistCustomerForm;
-
+/*
+    Combined concepts from accessingDataJPA and handlingFormSubmission with
+    Spring Data JDBC stack we used in LC101 unit 3
+    Goal is to save a customer to a sql table as simply as possible.
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
